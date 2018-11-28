8a3bea1da96dbabeaf29a28dfa95c74e8436c5c4
Gleb <lobzison@gmail.com>
2018-11-28 18:59:31 +0100

    Parsers

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,10 +1,7 @@
 package fpinscala.parsing
 
-
-
-
-
 import language.higherKinds
+import scala.util.matching.Regex
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
@@ -24,7 +21,7 @@ trait Parsers2[ParseError, Parser[+_]] {
 
 trait Parsers3[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
   def many[A](p: Parser[A]):Parser[List[A]]
 
@@ -35,14 +32,18 @@ trait Parsers3[ParseError, Parser[+_]] { self =>
   implicit def int(i: Int):Parser[Int]
   implicit def operators[A](p: Parser[A]):ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):ParserOps[String] = ParserOps(f(a))
+  implicit def regex(r: Regex): Parser[String]
   def listOfN[A](n: Int, p: Parser[A]):Parser[List[A]]
   def many[A](p: Parser[A]):Parser[List[A]]
-  def many1[A](p: Parser[A]): Parser[List[A]]
+  def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_::_)
   def succeed[A](a: A): Parser[A] = string("").map(_ => a)
+  def fail[A](a: A): Parser[A]
   def slice[A](p: Parser[A]): Parser[String]
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = p.flatMap(f andThen succeed)
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]):Parser[B]
+  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A,B)] = p1.flatMap(x => p2.map(y => (x,y))) // = map2(p1, p2)((_, _))
+  def map2[A,B,C](p1: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] = p1.flatMap(x => p2.map(y => f(x,y))) //map(product(p1, p2))(f.tupled)
 
 
   case class ParserOps[A](p: Parser[A]) {
@@ -52,6 +53,10 @@ trait Parsers3[ParseError, Parser[+_]] { self =>
     def flatMap[B](f: A => Parser[B]):Parser[B] = self.flatMap(p)(f)
     def many :Parser[List[A]] = self.many(p)
     def slice: Parser[String] = self.slice(p)
+    def many1: Parser[List[A]] = self.many1(p)
+    def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
+    def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+    def listOfN(n: Int) = self.listOfN(n, p)
 
   }
 
@@ -69,10 +74,19 @@ trait Parsers3[ParseError, Parser[+_]] { self =>
       forAll(in)(s => run(succeed(s))("") == Right(s))
     }
 
+    def productLaw[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop = {
+      ???
+      // product(success, success) == Right
+      // product(fail, success) == product(fail, fail) == Left
+      // product(success, fail) == Left
+    }
+
   }
 
   val numA:Parser[Int] = char('a').many.slice.map(_.length)
   def zeroOrMore(c: Char) = char(c).many.slice.map(_.length)
+  def oneFollowedBy(c: Char, c2: Char) = zeroOrMore(c) ** char(c2).many1.slice.map(_.length)
+  def nFollowedByNChar(c: Char):Parser[List[Char]] = "0-9".r.flatMap(x => char(c).listOfN(x.toInt))
 }
 
 case class ZeroOrMore[Int](c: Char) {
